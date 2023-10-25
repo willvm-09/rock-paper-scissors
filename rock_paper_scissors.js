e7d8b0dc2af5de50de0be2dab7dd78a49ec21309
William <williammutugi7@gmail.com>
2023-10-26 01:13:32 +0300

    Make function playerSelection case-insensitive

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -18,59 +18,65 @@ function getComputerChoice (min, max) {
 
 getComputerChoice(1, 3);
 
-//Create a similar function for the player
+//Create a similar function for the player. Use prompt() to get input from the user. 
 
 function getPlayerChoice () {
     const playerChoice = prompt ("Rock, Paper, or Scissors?");
     
-    if (playerChoice === "Rock" ) {
+    if (playerChoice.toLowerCase() === "rock" ) {
         return "Rock";
     }
-    if (playerChoice === "Paper") {
+    if (playerChoice.toLowerCase() === "paper") {
         return "Paper";
     }
-    if (playerChoice === "Scissors") {
+    if (playerChoice.toLowerCase() === "scissors") {
         return "Scissors";
     }
 }
 getPlayerChoice();
 
-/* Write a function that plays a single round of Rock Paper Scissors. 
+/* Write a function that plays a single round of Rock, Paper, Scissors. 
 The function should take two parameters - the playerSelection and computerSelection - 
 and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock" 
 Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation). */
 
 function playRound (playerSelection, computerSelection) {
 
-    if (playerSelection === "Rock" && computerSelection === "Rock") {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "Rock") {
         return "You both picked Rock, it's a tie!"
     }
-    if (playerSelection === "Paper" && computerSelection === "Paper") {
+    if (playerSelection.toLowerCase() === "paper" && computerSelection === "Paper") {
         return "You both picked Paper, it's a tie!"
     }
-    if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+    if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Scissors") {
         return "You both picked Scissors, it's a tie!"
     }
-    if (playerSelection === "Rock" && computerSelection === "Paper") {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") {
         return "You lose! Paper beats Rock."
     }
-    if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") {
         return "You win! Rock beats Scissors."
     }
-    if (playerSelection === "Paper" && computerSelection === "Rock") {
+    if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") {
         return "You win! Paper beats Rock."
     }
-    if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors") {
         return "You lose! Scissors beats Paper."
     }
-    if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") {
         return "You lose! Rock beats Scissors."
     }
-    if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper") {
         return "You win! Scissors beats Paper."
     }
 }
 
 playRound(getPlayerChoice(), getComputerChoice(1, 3));
 
+/* Write a NEW function called game(). 
+Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end. */
+
+    for (let i=1; i<=5; i++) {
+        playRound (i);
+    }
 
